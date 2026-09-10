50df27ec7014c6dc8db2daddc03de861e72353e9
Ajijolajesu <temzyajijolajesu@gmail.com>
2026-09-10 02:49:24 +0100

    Test single port connection with connect_ex

--- scanner.py
+++ scanner.py
@@ -9,3 +9,14 @@ if len (sys.argv) < 2:
     sys.exit(1)
 target = sys.argv[1]   #sys.argv allows type what we want to scan after typing the scanner.py 
 print (f"Target: {target}")
+
+port = 80 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a socket for IPV4 (AF_INET) using TCP(SOCK_STREAM)
+result = sock.connect_ex((target, port)) #tries to connect
+
+if result == 0: #Instead of crashing, it returns 0 for success 
+    print(f"port {port} is open")
+else:
+    print(f"port {port} is closed") #and a non-zero for failure
+
+sock.close() #frees the connection; closes it once were done with it
